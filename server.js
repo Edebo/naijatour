602ebb9a4162d9ce641e4fc5ed26af9e41ebf2be
Edebo <akandeadebowale0@gmail.com>
2019-09-11 15:15:22 +0100

    adding .gitignore

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const app = require('./app')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./app');
 
-process.on('uncaughtException',err=>{
-    console.log("uncaught exception handling")
-    console.log(err.name,err.message)
-    //server.close helps to finish handling all request before shutting down
-    
-        console.log("handling Unhandled rejection. Now shutting down ... application")
-        process.exit(1)
-})
+process.on('uncaughtException', err => {
+  console.log('uncaught exception handling');
+  console.log(err.name, err.message);
+  //server.close helps to finish handling all request before shutting down
+
+  console.log(
+    'handling Unhandled rejection. Now shutting down ... application'
+  );
+  process.exit(1);
+});
 
 //database connection
-const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
-mongoose.connect(DB,{
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useFindAndModify:false
-}).then(()=>{
-    
-    console.log("connection to database is successful")
-}).catch(err=>{
-    console.log('connection to database failed')
-})
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('connection to database is successful');
+  })
+  .catch(err => {
+    console.log('connection to database failed');
+  });
 
-
-const PORT=process.env.PORT || 3000
-const server = app.listen(PORT,()=>{
-    console.log('Server started successfully')
-   
-})
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log('Server started successfully');
+});
 
 //this is for unhandled error:=>error outside express
-process.on('unhandledRejection',err=>{
-    console.log("Unhandled rejection")
-    console.log(err.name,err.message)
-    //server.close helps to finish handling all request before shutting down
-    server.close(()=>{
-        console.log("handling Unhandled rejection. Now shutting down ... application")
-        process.exit(1)
-    })
-
-})
-
+process.on('unhandledRejection', err => {
+  console.log('Unhandled rejection');
+  console.log(err.name, err.message);
+  //server.close helps to finish handling all request before shutting down
+  server.close(() => {
+    console.log(
+      'handling Unhandled rejection. Now shutting down ... application'
+    );
+    process.exit(1);
+  });
+});
 
 //this is handling uncaught error
-
